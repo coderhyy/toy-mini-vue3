26bdd22db7f023a486412d0163cec76235f1816a
hyy <1014873097@qq.com>
2022-06-30 15:39:11 +0800

    test: 新增 toRaw、isProxy 测试用例

--- src/reactivity/test/reactive.spec.ts
+++ src/reactivity/test/reactive.spec.ts
@@ -1,4 +1,4 @@
-import { reactive, isReactive } from "../reactive";
+import { reactive, isReactive, isProxy, toRaw } from "../reactive";
 
 describe("reactive", () => {
   it("reactive test", () => {
@@ -8,6 +8,7 @@ describe("reactive", () => {
     expect(count.num).toEqual(1);
     expect(isReactive(original)).toBe(false);
     expect(isReactive(count)).toBe(true);
+    expect(isProxy(count)).toBe(true);
   });
   it("nested reactive", () => {
     let original = {
@@ -22,5 +23,23 @@ describe("reactive", () => {
     expect(isReactive(nested.arr[0])).toBe(true);
     expect(isReactive(nested.foo)).toBe(true);
     // expect(isReactive(nested.foo.name)).toBe(true) // 涉及到往后的知识点 isRef
+  });
+  test("toRaw", () => {
+    const original = { foo: 1 };
+    const observed = reactive(original);
+    // 输出的结果必须要等于原始值
+    expect(toRaw(observed)).toBe(original);
+    expect(toRaw(original)).toBe(original);
+  });
+  it("nested reactive toRaw", () => {
+    const original = {
+      foo: {
+        name: "hyy",
+      },
+    };
+    const observed = reactive(original);
+    const raw = toRaw(observed);
+    expect(raw).toBe(original);
+    expect(raw.foo).toBe(original.foo);
   });
 });
