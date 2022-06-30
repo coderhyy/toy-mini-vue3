c9d81db92bcb681a063a2e05ec540ecf4529297b
hyy <1014873097@qq.com>
2022-06-30 15:38:21 +0800

    feat: 新增 toRaw

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -10,12 +10,14 @@ export enum ReactiveFlags {
   IS_REACTIVE = "__v_isReactive",
   IS_READONLY = "__v_isReadonly",
   IS_SHALLOW = "__v_isShallow",
+  RAW = "__v_raw",
 }
 
 export interface Target {
   [ReactiveFlags.IS_REACTIVE]?: boolean;
   [ReactiveFlags.IS_READONLY]?: boolean;
   [ReactiveFlags.IS_SHALLOW]?: boolean;
+  [ReactiveFlags.RAW]?: any;
 }
 
 export function reactive<T extends object>(target: T) {
@@ -49,4 +51,9 @@ export function isShallow(value: unknown) {
 
 export function isProxy(value: unknown) {
   return isReactive(value) || isReadonly(value);
+}
+
+export function toRaw<T>(observed: T): T {
+  const raw = observed && (observed as Target)[ReactiveFlags.RAW];
+  return raw ? toRaw(raw) : observed;
 }
