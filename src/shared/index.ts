36fe2128c62b11faa2d050c4e674031fc24d33cd
hyy <1014873097@qq.com>
2022-07-01 18:15:29 +0800

    feat：新增 isFunction 函数

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -5,3 +5,8 @@ export const isObject = (value: unknown) => {
 export const hasChanged = (value: unknown, oldValue: unknown) => {
   return !Object.is(value, oldValue);
 };
+
+// 类型保护
+export const isFunction = (value: unknown): value is Function => {
+  return typeof value === "function";
+};
